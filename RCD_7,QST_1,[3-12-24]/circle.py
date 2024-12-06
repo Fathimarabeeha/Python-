3d2f5af50f7ea118eff390fb1edb377604f56567
Fathimarabeeha <fathimarabeehakp@gmail.com>
2024-12-06 14:17:14 +0530

    Delete RCD_7,QST_1,[3-12-24] directory

--- RCD_7,QST_1,[3-12-24]/circle.py
+++
@@ -1,5 +0,0 @@
-def circlearea (r):
-    print("Area of Circle: ",3.14*r*r);
-def circleperi (r):
-    print("Perimeter of Circle: ",2*3.14*r);
-    
--- RCD_7,QST_1,[3-12-24]/rectangle.py
+++
@@ -1,4 +0,0 @@
-def rectarea (a,b):
-    print("Area of Rectangle: ",a*b);
-def rectperi (a,b):
-    print("Perimeter of Rectangle: ",2*(a+b));
