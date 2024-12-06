1d2a3274b780fea7b7f326e7041c35dc841299ba
Fathimarabeeha <fathimarabeehakp@gmail.com>
2024-12-06 14:16:41 +0530

    Delete RCD_7,QST_2,[3-12-24] directory

--- RCD_7,QST_2,[3-12-24]/ksb/ABC/cuboid.py
+++
@@ -1,4 +0,0 @@
-def cuboidarea (l,b,h):
-    print("Area of cuboid: ",2*((l*b)+(l*h)+(b*h)))
-def cuboidperi (l,b,h):
-    print("Perimeter of cuboid: ",4*(l+b+h))
--- RCD_7,QST_2,[3-12-24]/ksb/rectangle.py
+++
@@ -1,4 +0,0 @@
-def rectarea (l,b):
-    print("Area of Rectangle: ",l*b)
-def rectperi (l,b):
-    print("Perimeter of rectangle: ",2*(l+b))
